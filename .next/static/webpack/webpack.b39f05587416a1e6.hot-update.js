adc8124e51c30c860569a3f29c4a154dcff6c376
Brenda Pereira Camara <brendacamara@outlook.com>
2022-03-16 13:51:33 -0300

    chore: build verison 1

--- .next/static/webpack/webpack.b39f05587416a1e6.hot-update.js
+++ .next/static/webpack/webpack.b39f05587416a1e6.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "a185597b5250f6d3"; }
+/******/ 	__webpack_require__.h = function() { return "904f2ff6759c4658"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/make namespace object */
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
+
+function Title(props){
+    const Tag = props.tag
+    return (
+        <>
+            <Tag>{props.children}</Tag>
+            <style jsx>{`
+                ${Tag} {
+                    color: green;
+                }
+            `}
+            </style>
+        </>
+    )
+}
+
+
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
-  }
+    return (
+            <div>
+                <Title tag="h2">Bem-vindo de volta!</Title>
+                <h2>Alura matrix</h2>
+            </div>
+        )
+        }
   
   export default HomePage
